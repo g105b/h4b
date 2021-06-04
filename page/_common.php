73aa7d4e8c30921af7f89b0d1697afd48baacf47
Greg Bowler <greg.bowler@g105b.com>
2021-06-04 19:20:43 +0100

    auto select menu items

--- page/_common.php
+++ page/_common.php
@@ -7,7 +7,26 @@ use H4B\Content\ContentRepository;
 
 class _CommonPage extends Page {
 	public function go():void {
+		$this->selectMenu();
 		$this->outputContent();
+	}
+
+	private function selectMenu():void {
+		$uriPath = $this->server->getRequestUri()->getPath();
+		foreach($this->document->querySelectorAll("body>header nav li a") as $anchor) {
+			if(str_starts_with($uriPath, $anchor->href)) {
+				if($anchor->href === "/") {
+					if($anchor->href === $uriPath) {
+						$anchor->parentNode->classList->add("selected");
+					}
+				}
+				else {
+					if(str_starts_with($uriPath, $anchor->href)) {
+						$anchor->parentNode->classList->add("selected");
+					}
+				}
+			}
+		}
 	}
 
 	private function outputContent():void {
