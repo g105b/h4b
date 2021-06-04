e4ffa30c95bb48ff2f4708b487dfadc19f158171
Greg Bowler <greg.bowler@g105b.com>
2021-06-04 21:15:09 +0100

    wip: responsive images

--- page/_common.php
+++ page/_common.php
@@ -1,14 +1,19 @@
 <?php
 namespace H4B\Page;
 
+use Gt\Dom\Text;
 use Gt\DomTemplate\Element;
+use Gt\WebEngine\FileSystem\Path;
 use Gt\WebEngine\Logic\Page;
 use H4B\Content\ContentRepository;
+use League\CommonMark\HtmlElement;
 
 class _CommonPage extends Page {
 	public function go():void {
 		$this->selectMenu();
 		$this->outputContent();
+		$imageContainerList = $this->imageContainers();
+		$this->imageSourceSet($imageContainerList);
 	}
 
 	private function selectMenu():void {
@@ -38,5 +43,75 @@ class _CommonPage extends Page {
 			$html = $contentRepo->getHTML($name);
 			$contentElement->innerHTML = $html;
 		}
+	}
+
+	/** @return HtmlElement[] HTMLParagraphElement[] */
+	private function imageContainers():array {
+		$imageContainerList = [];
+
+		foreach($this->document->querySelectorAll("article>p") as $paragraph) {
+			if($paragraph->classList->contains("float")) {
+				continue;
+			}
+			foreach($paragraph->childNodes as $child) {
+				if($child instanceof Text) {
+					if(trim($child->nodeValue) !== "") {
+						continue(2);
+					}
+				}
+				elseif($child->tagName !== "img") {
+					continue(2);
+				}
+			}
+
+			array_push($imageContainerList, $paragraph);
+		}
+
+		foreach($imageContainerList as $container) {
+			$container->classList->add("image-container");
+		}
+
+		return $imageContainerList;
+	}
+
+	private function imageSourceSet(array $imageContainerList):void {
+		foreach($imageContainerList as $container) {
+			foreach($container->querySelectorAll("img") as $img) {
+				$this->imageSourceSetApply($img);
+			}
+		}
+	}
+
+	private function imageSourceSetApply(Element $img):void {
+		$src = urldecode($img->src);
+		$sizeList = [200, 800];
+
+		$srcsetArray = [];
+		$sizesArray = [];
+
+		foreach($sizeList as $size) {
+			$doubleSize = $size * 2;
+
+			$sizeSrc = str_replace(
+				"/photo/",
+				"/photo/thumb-$size/",
+				$src
+			);
+
+			$fullPathSrc = Path::getApplicationRootDirectory(__DIR__) . $src;
+			$fullPathSizeSrc = Path::getApplicationRootDirectory(__DIR__) . $sizeSrc;
+
+			if(!file_exists($fullPathSizeSrc)) {
+//				$this->createImageSource($fullPathSrc, $fullPathSizeSrc);
+			}
+
+//			$sizeSrcUrlEncode = urlencode($sizeSrc);
+
+			array_push($srcsetArray, "${sizeSrc} ${size}w");
+			array_push($sizesArray, "(max-width: ${doubleSize}px) ${size}px");
+		}
+
+		$img->setAttribute("srcset", implode(",", $srcsetArray));
+		$img->setAttribute("sizes", implode(",", $sizesArray));
 	}
 }
