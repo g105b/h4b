211c39cce65b22be41775ab34142ae2a6a81aa4a
Greg Bowler <greg.bowler@g105b.com>
2021-06-10 15:49:11 +0100

    image popups

---
+++ script/images.js
@@ -0,0 +1,41 @@
+document.querySelectorAll("img[srcset]").forEach(function(img) {
+	img.addEventListener("click", openPopup);
+});
+
+let currentPopup = null;
+
+function openPopup() {
+	let popup = document.createElement("div");
+	popup.classList.add("popup");
+	let img = this.cloneNode();
+	img.removeAttribute("srcset");
+	location.hash = img.src;
+
+	popup.addEventListener("click", closePopup);
+	window.addEventListener("keydown", closePopup);
+	window.addEventListener("hashchange", checkHash);
+
+	currentPopup = popup;
+	popup.appendChild(img);
+	document.body.appendChild(popup);
+}
+
+function checkHash(e) {
+	if(location.hash.length <= 2) {
+		closePopup();
+	}
+}
+
+function closePopup(e) {
+	if(e
+	&& e.key
+	&& e.key !== "Escape") {
+		return;
+	}
+
+	window.removeEventListener("keypress", closePopup);
+	window.removeEventListener("hashchange", closePopup)
+	currentPopup.remove();
+	currentPopup = null;
+	location.hash = "_";
+}
